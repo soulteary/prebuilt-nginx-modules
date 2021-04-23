157323973d3c3bdcd394f1893c08b2e512f8581a
soulteary <soulteary@gmail.com>
2021-04-23 15:49:01 +0800

    add debian base image

--- make-base.sh
+++ make-base.sh
@@ -7,11 +7,24 @@ set -a
     . "$RELEASE_DIR/.env"
 set +a
 
+# Build For Alpine
+
 TAG=base-$NGINX_VERSION-alpine;
 DIST=$REPO_NAME:$TAG
 
 echo "Build: $DIST";
 
 echo $NGINX_VERSION:$NGINX_SHASUM
-echo "docker build --build-arg NGINX_VERSION=$NGINX_VERSION --build-arg NGINX_SHASUM=$NGINX_SHASUM --tag $DIST -f "$RELEASE_DIR/Dockerfile" ."
-docker build --build-arg NGINX_VERSION="$NGINX_VERSION" --build-arg NGINX_SHASUM="$NGINX_SHASUM" --tag $DIST -f "$RELEASE_DIR/Dockerfile" .
+echo "docker build --build-arg NGINX_VERSION=$NGINX_VERSION --build-arg NGINX_SHASUM=$NGINX_SHASUM --tag $DIST -f "$RELEASE_DIR/Dockerfile.alpine" ."
+docker build --build-arg NGINX_VERSION="$NGINX_VERSION" --build-arg NGINX_SHASUM="$NGINX_SHASUM" --tag $DIST -f "$RELEASE_DIR/Dockerfile.alpine" .
+
+# Build For Debian
+
+TAG=base-$NGINX_VERSION;
+DIST=$REPO_NAME:$TAG
+
+echo "Build: $DIST";
+
+echo $NGINX_VERSION:$NGINX_SHASUM
+echo "docker build --build-arg NGINX_VERSION=$NGINX_VERSION --build-arg NGINX_SHASUM=$NGINX_SHASUM --tag $DIST -f "$RELEASE_DIR/Dockerfile.debian" ."
+docker build --build-arg NGINX_VERSION="$NGINX_VERSION" --build-arg NGINX_SHASUM="$NGINX_SHASUM" --tag $DIST -f "$RELEASE_DIR/Dockerfile.debian" .
