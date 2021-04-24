6af1c5d0de1084a859a795310b92af9f8de7870d
soulteary <soulteary@gmail.com>
2021-04-24 13:13:04 +0800

    add debian build script

--- make-image.sh
+++ make-image.sh
@@ -4,14 +4,27 @@ RELEASE_DIR="./modules";
 REPO_NAME="soulteary/prebuilt-nginx-modules"
 
 for moduleName in $RELEASE_DIR/*; do
+
     set -a
         . "$moduleName/.env"
     set +a
 
     tag=$(echo $moduleName | cut -b 11-);
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
-    echo "Build: $BUILD_NAME";
-    BUILD_ARGS=$(tr '\n' ';' < "$moduleName/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
 
-    docker build $BUILD_ARGS --tag $BUILD_NAME -f $moduleName/Dockerfile.alpine .
+    if [ -f "$moduleName/Dockerfile.alpine" ]; then
+        BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
+        echo "Build: $BUILD_NAME";
+        BUILD_ARGS=$(tr '\n' ';' < "$moduleName/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
+
+        docker build $BUILD_ARGS --tag $BUILD_NAME -f $moduleName/Dockerfile.alpine .
+    fi
+
+    if [ -f "$moduleName/Dockerfile.debian" ]; then
+        BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION"
+        echo "Build: $BUILD_NAME";
+        BUILD_ARGS=$(tr '\n' ';' < "$moduleName/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
+
+        docker build $BUILD_ARGS --tag $BUILD_NAME -f $moduleName/Dockerfile.debian .
+    fi
+
 done
