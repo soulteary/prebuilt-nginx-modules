135a84ab91c6d9b4f8626b2664a2079d55187b31
soulteary <soulteary@gmail.com>
2021-04-24 13:33:00 +0800

    upd push script

--- push-image.sh
+++ push-image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 REPO_NAME="soulteary/prebuilt-nginx-modules"
-
 RELEASE_DIR="./baseImage";
+
 set -a
     . "$RELEASE_DIR/.env"
 set +a
@@ -10,8 +10,10 @@ set +a
 TAG="base-$NGINX_VERSION-alpine";
 DIST="$REPO_NAME:$TAG"
 
-echo "Push: $DIST";
-docker push DIST;
+if [[ "$(docker images -q $DIST 2> /dev/null)" != "" ]]; then
+    echo "Push: $DIST";
+    docker push $DIST;
+fi
 
 RELEASE_DIR="./modules";
 
@@ -21,7 +23,21 @@ for moduleName in $RELEASE_DIR/*; do
     set +a
 
     tag=$(echo $moduleName | cut -b 11-);
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
-    echo "Push: $BUILD_NAME";
-    docker push $BUILD_NAME;
+
+    if [ -f "$moduleName/Dockerfile.alpine" ]; then
+        BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
+            echo "Push: $BUILD_NAME";
+            docker push $BUILD_NAME;
+        fi
+    fi
+
+    if [ -f "$moduleName/Dockerfile.debian" ]; then
+        BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
+            echo "Push: $BUILD_NAME";
+            docker push $BUILD_NAME;
+        fi
+    fi
+
 done
